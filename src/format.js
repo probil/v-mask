ae23140e5cd91af11e10a3d0200ecc442764effb
Adam Bright <brightam1@gmail.com>
2017-02-10 11:24:02 -0500

    allow first mask character to not be special

--- src/format.js
+++ src/format.js
@@ -15,6 +15,11 @@ export default function (data, mask){
     let c = data.charAt(i);
     let m = mask.charAt(i);
 
+    if (data.length == 1 && i == 0 && i + 1 < mask.length && /^((?!(#|A|N|X)).)*/.test(m)) {
+        text += m;
+        m = mask.charAt(i + 1);
+    }
+
     switch (m) {
       case '#' : if (/\d/.test(c))        {text += c;} else {x = 0;} break;
       case 'A' : if (/[a-z]/i.test(c))    {text += c;} else {x = 0;} break;
