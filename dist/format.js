eff2a9d4949f1d3a8c9afc7684627144e9dc7527
Max Lyashuk <m.liashuk@zendos.org>
2017-02-13 02:14:22 +0200

    Bump version 1.0.3

--- dist/format.js
+++ dist/format.js
@@ -13,8 +13,8 @@ exports.default = function (data, mask) {
     var m = mask.charAt(i);
 
     if (data.length == 1 && i == 0 && i + 1 < mask.length && /^((?!(#|A|N|X)).)*/.test(m)) {
-        text += m;
-        m = mask.charAt(i + 1);
+      text += m;
+      m = mask.charAt(i + 1);
     }
 
     switch (m) {
