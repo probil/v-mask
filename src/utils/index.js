d4d821af0ca58a53c0a717e86be3e0d04511885f
Max Lyashuk <m.liashuk@zendos.org>
2017-03-06 02:48:31 +0200

    [#16] [#18] Fixed issues with `v-model`
    - Fixed issue with excessive chars inside model data
    - Simplified directive by removing custom event listeners

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+/**
+ * Notifies Vue about internal value change
+ * @see https://github.com/vuejs/Discussion/issues/157#issuecomment-273301588
+ *
+ * @param {HTMLInputElement} el
+ * @param {String}           type
+ */
+export const trigger = (el, type) =>  {
+  const e = document.createEvent('HTMLEvents');
+  e.initEvent(type, true, true);
+  el.dispatchEvent(e)
+};
