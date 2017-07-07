adf7ec377f5cc9160b77f8d3cd049d5782a5bd2d
shrpne <shrpne@gmail.com>
2017-07-07 16:16:28 +0300

    make possible to use v-mask as directive

--- src/index.js
+++ src/index.js
@@ -29,47 +29,55 @@ function updateMask(el, mask) {
 
 
 /**
+ * Vue directive definition
+ */
+const VueMaskDirective = {
+
+  /**
+   * Called only once, when the directive is first bound to the element.
+   * This is where you can do one-time setup work.
+   *
+   * @param {HTMLInputElement} el
+   * @param {?String}          value
+   */
+  bind (el, {value}) {
+    updateMask(el, value);
+    updateValue(el);
+  },
+
+  /**
+   * Called after the containing component has updated,
+   * but possibly before its children have updated.
+   * The directive’s value may or may not have changed,
+   * but you can skip unnecessary updates by comparing the
+   * binding’s current and old values.
+   *
+   * @param {HTMLInputElement} el
+   * @param {?String}          value
+   * @param {?String}          oldValue
+   */
+  componentUpdated(el, {value, oldValue}){
+
+    let isMaskChanged = value !== oldValue;
+
+    // update mask first if changed
+    if(isMaskChanged){
+      updateMask(el, value);
+    }
+
+    // update value
+    updateValue(el, isMaskChanged);
+  }
+};
+
+
+/**
  * Vue plugin definition
  * @param {Vue} Vue
  */
-export default function (Vue) {
-  Vue.directive('mask', {
-
-    /**
-     * Called only once, when the directive is first bound to the element.
-     * This is where you can do one-time setup work.
-     *
-     * @param {HTMLInputElement} el
-     * @param {?String}          value
-     */
-    bind (el, {value}) {
-      updateMask(el, value);
-      updateValue(el);
-    },
-
-    /**
-     * Called after the containing component has updated,
-     * but possibly before its children have updated.
-     * The directive’s value may or may not have changed,
-     * but you can skip unnecessary updates by comparing the
-     * binding’s current and old values.
-     *
-     * @param {HTMLInputElement} el
-     * @param {?String}          value
-     * @param {?String}          oldValue
-     */
-    componentUpdated(el, {value, oldValue}){
-
-      let isMaskChanged = value !== oldValue;
-
-      // update mask first if changed
-      if(isMaskChanged){
-        updateMask(el, value);
-      }
-
-      // update value
-      updateValue(el, isMaskChanged);
-    }
-  });
+const VueMaskPlugin = function (Vue) {
+  Vue.directive('mask', VueMaskDirective);
 };
+
+export { VueMaskPlugin as default, VueMaskDirective };
 
