d26f20ea05ced8293df924b39e3969a51f25ab12
Max Lyashuk <m_lyashuk@ukr.net>
2018-02-27 11:44:16 +0200

    tests: add tests for plugin/directive registration

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,25 +1,59 @@
-import vueMask from '../index';
+import { createLocalVue, mount } from '@vue/test-utils';
+import VueMask, { VueMaskDirective, VueMaskPlugin } from '../index';
 
-describe('Element binding works', () => {
-  let bindFunc = function bindFunc() {};
-  const mockVue = {};
+describe('plugin/directive registration', () => {
+  let Vue;
 
   beforeEach(() => {
-    global.document = {
-      createEvent() {
-        return {
-          initEvent() {},
-        };
-      },
-    };
+    Vue = createLocalVue();
+  });
+
+  it('default export should be a function', () => {
+    expect(VueMask).toEqual(expect.any(Function));
+  });
+
+  it('named export `VueMaskPlugin` should be a function', () => {
+    expect(VueMaskPlugin).toEqual(expect.any(Function));
+  });
+
+  it('named export `VueMaskDirective` should be an object', () => {
+    expect(VueMaskDirective).toEqual(expect.any(Object));
+  });
+
+  it('should register `v-mask` directive', () => {
+    expect(Vue.options.directives.mask).toBeUndefined();
+    Vue.use(VueMask);
+    expect(Vue.options.directives.mask).toEqual(expect.any(Object));
+  });
+
+  it('should allow to use exposed directive with custom name', () => {
+    expect(Vue.options.directives.fakeMask).toBeUndefined();
+    Vue.directive('fakeMask', VueMaskDirective);
+    expect(Vue.options.directives.fakeMask).toEqual(expect.any(Object));
+  });
+});
+
+describe('directive usage', () => {
+  let Vue;
+  beforeAll(() => {
+    Vue = createLocalVue();
+    Vue.use(VueMask);
+  });
+
+  it('should run this canary test', () => {
+    const wrapper = mount({
+      template: '<input />',
+    });
+    expect(wrapper.is('input')).toBe(true);
+  });
+
+  test('Should set value when bound and previous value is `undefined`', () => {
+    let bindFunc = function bindFunc() {};
+    const mockVue = {};
     mockVue.directive = function directive(name, funcObject) {
       bindFunc = funcObject.bind;
     };
-    vueMask(mockVue);
-  });
-
-
-  test('Should set value when bound and previous value is `undefined`', () => {
+    VueMask(mockVue);
     const mask = { value: '##.##.####' };
     const element = {
       value: '11112011',
