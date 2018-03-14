9c71a81cf935b71360ead2640c2dabab8f06d5eb
Max Lyashuk <m_lyashuk@ukr.net>
2018-03-14 16:28:28 +0200

    feat(interface): expose VMaskWrapper component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import directive from './directive';
 import plugin from './plugin';
+import VMaskWrapper from './VMaskWrapper';
 
 export {
   plugin as default,
   plugin as VueMaskPlugin,
   directive as VueMaskDirective,
+  VMaskWrapper as VueMaskWrapper,
 };
