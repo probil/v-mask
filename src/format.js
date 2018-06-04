53d60a18737199be960d7a7fd32277b46cda1785
Max Lyashuk <m_lyashuk@ukr.net>
2018-06-04 16:44:32 +0300

    refactor(format): improve variable naming

--- src/format.js
+++ src/format.js
@@ -4,29 +4,29 @@
  * Simple format function borrowed from PureMask.js
  * {@link https://github.com/romulobrasil/PureMask.js}
  *
- * @param {String} data String to mask (input value)
- * @param {String} [mask] Mask format, like `####-##`
+ * @param {String} text String to mask (input value)
+ * @param {String} [wholeMask] Mask format, like `####-##`
  * @returns {string} Formatted text
  */
-export default function (data, mask) {
+export default function (text, wholeMask) {
   // don't do anything if mask is undefined/null/etc
-  if (!mask) return data;
+  if (!wholeMask) return text;
 
   const maskStartRegExp = /^([^#ANX]+)/;
 
-  if (+data.length === 1 && maskStartRegExp.test(mask)) {
-    data = maskStartRegExp.exec(mask)[0] + data;
+  if (+text.length === 1 && maskStartRegExp.test(wholeMask)) {
+    text = maskStartRegExp.exec(wholeMask)[0] + text;
   }
 
-  let text = '';
+  let newText = '';
 
   // Adds a char offset to allow testing on optional values
   let cOffset = 0;
 
   // Cleans data to  avoid value loss on dynamic mask changing
-  for (let i = 0; i < mask.length; i += 1) {
-    const m = mask.charAt(i);
-    switch (m) {
+  for (let i = 0; i < wholeMask.length; i += 1) {
+    const mask = wholeMask.charAt(i);
+    switch (mask) {
       case '#':
         break;
       case 'A':
@@ -38,42 +38,42 @@ export default function (data, mask) {
       case 'X':
         break;
       default:
-        data = data.replace(m, '');
+        text = text.replace(mask, '');
     }
   }
-  for (let i = 0, x = 1; x && i < mask.length; i += 1) {
+  for (let i = 0, x = 1; x && i < wholeMask.length; i += 1) {
     // Uses the optional mask character offset
-    const c = data.charAt(i - cOffset);
-    const m = mask.charAt(i);
+    const char = text.charAt(i - cOffset);
+    const mask = wholeMask.charAt(i);
 
-    switch (m) {
+    switch (mask) {
       case '#':
-        /\d/.test(c) ? text += c : x = 0;
+        /\d/.test(char) ? newText += char : x = 0;
         break;
       case 'A':
-        /[a-z]/i.test(c) ? text += c : x = 0;
+        /[a-z]/i.test(char) ? newText += char : x = 0;
         break;
       case 'N':
-        /[a-z0-9]/i.test(c) ? text += c : x = 0;
+        /[a-z0-9]/i.test(char) ? newText += char : x = 0;
         break;
       // Skips testing if optional field is specified
       case '?':
         cOffset += 1;
         break;
       case 'X':
-        text += c;
+        newText += char;
         break;
       default:
-        text += m;
+        newText += mask;
 
         // preserve characters that are in the same spot we need to insert a mask
         // character by shifting the data over to the right (issue #5, & #7)
-        if (c && c !== m) {
-          data = ` ${data}`;
+        if (char && char !== mask) {
+          text = ` ${text}`;
         }
 
         break;
     }
   }
-  return text;
+  return newText;
 }
