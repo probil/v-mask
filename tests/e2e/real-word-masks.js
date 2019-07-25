13eb4e0d0ff988f27f3d089ad9509128335c97b4
Max Lyashuk <max@onsweb.nl>
2019-07-25 04:01:05 +0300

    style(e2e): reformat e2e tests according to eslint config

--- tests/e2e/real-word-masks.js
+++ tests/e2e/real-word-masks.js
@@ -1,37 +1,38 @@
 import { Selector } from 'testcafe';
 
-fixture `Real-world masks`
-  .page `./index.html`;
+// eslint-disable-next-line no-unused-expressions
+fixture`Real-world masks`
+  .page`./index.html`;
 
-test('Time and date input', async t => {
+test('Time and date input', async (t) => {
   const el = Selector('input#time-and-date-ex');
   await t
     .typeText(el, '271020162315')
     .expect(el.value).eql('27/10/2016 23:15');
 });
 
-test('Time input', async t => {
+test('Time input', async (t) => {
   const el = Selector('input#time-ex');
   await t
     .typeText(el, '111515')
     .expect(el.value).eql('11:15:15');
 });
 
-test('Credit card input', async t => {
+test('Credit card input', async (t) => {
   const el = Selector('input#credit-cart-ex');
   await t
     .typeText(el, '4444444444444444')
     .expect(el.value).eql('4444 4444 4444 4444');
 });
 
-test('Phone number input', async t => {
+test('Phone number input', async (t) => {
   const el = Selector('input#phone-number-ex');
   await t
     .typeText(el, '9999999999')
     .expect(el.value).eql('(999) 999-9999');
 });
 
-test('US phone number', async t => {
+test('US phone number', async (t) => {
   const el = Selector('input#us-phone-number-ex');
   await t
     .typeText(el, '9999999999')
